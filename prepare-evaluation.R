5eba455180d995dc3965dba11a9abb4ad4a2fc2c
Jeff Oliver <jcoliver@email.arizona.edu>
2021-04-07 17:11:53 -0700

    Add entry processing script

---
+++ prepare-evaluation.R
@@ -0,0 +1,48 @@
+# Process entries for evaluation
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2021-04-07
+
+library(tidyverse)
+library(rmarkdown)
+
+# Create
+# 1. Sheet for scoring entries, (or part of it) with FiLa as column heading, 
+#    where FiLa is the first two initials of First and Last names, respectively
+# 2. Single PDF with one entry per page
+#    For this to work, a couple of notes:
+#    a. Had to use the xelatex engine (instead of pdflatex?) to get some 
+#       symbols in abstracts to display 
+#    b. To insert images into the pdf, I downloaded all the images, stuck them
+#       in data/images, and renamed those that could be inserted. That is, any 
+#       files that were single, static images of acceptable format were renamed 
+#       to FiLa.* (acceptable values of *: png, jpg). PDF and gif files were 
+#       *not* renamed, prompting a message that they should be viewed online.
+
+responses <- readr::read_csv(file = "data/responses.csv")
+
+# Timestamp needs to be converted from character
+responses$Timestamp <- lubridate::as_datetime(x = responses$Timestamp,
+                                              format = "%m/%d/%Y %H:%M:%S",
+                                              tz = "MST")
+
+# Drop duplicate entries, retaining most recent entry only
+# responses <- responses
+uniq_responses <- responses %>%
+  arrange(desc(Timestamp)) %>%
+  distinct(`First Name`, `Last Name`, .keep_all = TRUE) %>%
+  arrange(`First Name`)
+
+
+# 1. Sheet for evaluations
+initials <- paste0(substr(x = uniq_responses$`First Name`, start = 1, stop = 2),
+                   substr(x = uniq_responses$`Last Name`, start = 1, stop = 2))
+eval_sheet <- data.frame(matrix(nrow = 1, ncol = length(initials)))
+names(eval_sheet) <- initials
+readr::write_csv(x = eval_sheet, file = "output/evaluations.csv")
+
+# 2. PDF with one entry per page
+rmarkdown::render(input = "Entries.Rmd", 
+                  # output_format = "pdf_document",
+                  output_file = "output/Entries",
+                  params = list(responses = uniq_responses))
